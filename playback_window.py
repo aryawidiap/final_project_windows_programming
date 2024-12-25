5eff3ae410e2bc5daddb5c1758540030712a7213
Abiansyah Gymnastiar <abiansyahadzanigymnas1012@gmail.com>
2024-12-25 11:46:54 +0800

    yuhh

--- playback_window.py
+++ playback_window.py
@@ -21,31 +21,42 @@ class PlayBackWindow(tk.Frame):
         row_number = 0
         title_label = tk.Label(self,text='Playback')
         title_label.grid(column=0,row=row_number)
+
+        row_number += 1
+        control_frame = tk.Frame(self)
+        control_frame.grid(column=0, row=row_number, columnspan=3, )
+        upload_button = tk.Button(control_frame, text="Upload File", command=self.open_file, height=5, width=10)
+        upload_button.grid(column=0, row=0)
+        self.play_button = tk.Button(control_frame, text="Play", command=self.start_threading, height=5, width=10)
+        self.play_button.grid(column=1, row=0)
+        stop_button = tk.Button(control_frame, text="Stop", command=self.stop_threading, height=5, width=10)
+        stop_button.grid(column=2, row=0)
+        self.is_playing = False
+        self.is_paused = True
+
         row_number += 1
         picture_frame = tk.Frame(self,width=200, height=100)
         picture_frame.grid(column=0,row=row_number)
         row_number += 1
         self.gender_instrument = GenderInstrument(self)
         self.gender_instrument.grid(column=0,row=row_number)
-        row_number += 1
-        upload_button = tk.Button(self, text="Upload File", command=self.open_file)
-        upload_button.grid(column=0, row=row_number)
-        row_number += 1
-        ## playback button
-        self.play_button = tk.Button(self, text="Play Back", command=self.start_threading)
-        self.play_button.grid(column=0, row=row_number)
-        self.is_playing = False
 
     def start_threading(self):
-        if self.is_playing:
-            self.play_button.config(text="Play Back")
-            self.is_playing = False
+        if self.is_paused:
+            self.play_button.config(text="Pause")
+            self.is_paused = False
+            if not self.is_playing:
+                playback_thread = threading.Thread(target=self.playback)
+                playback_thread.start()
         else:
-            self.play_button.config(text="Stop")
-            self.is_playing = True
-            playback_thread = threading.Thread(target=self.playback)
-            playback_thread.start()
-        
+            self.play_button.config(text="Resume")
+            self.is_paused = True
+    
+    def stop_threading(self):
+        self.is_playing = False
+        self.is_paused = True
+        self.play_button.config(text="Play")
+
     def __play_sound(self, key):
         print(key)
         if key != -1:
@@ -56,23 +67,26 @@ class PlayBackWindow(tk.Frame):
 
     def playback(self):
         self.is_playing = True
-        for note in self.note_ranges:
-            if not self.is_playing:
-                break
-            self.__play_sound(note)
-            sleep(0.5)
+        index = 0
+        while self.is_playing:
+            if not self.is_paused:
+                self.__play_sound(self.note_ranges[index])
+                index += 1
+                if index >= len(self.note_ranges):
+                    self.is_playing = False
+                sleep(0.5)
+        self.is_paused = True
         self.is_playing = False
         self.play_button.config(text="Play Back")
     
     def open_file(self):
-        file = askopenfile(mode = 'r')
+        self.stop_threading()
+        file = askopenfile(mode = 'r', filetypes=[('Text Files', '*.txt')])
+
         if file is not None:
             content = file.read()
             print(content)
             self.note_ranges = list(map(int, content.split(',')))
-            
-
-            
 
 root = tk.Tk()
 playback_window = PlayBackWindow(root)

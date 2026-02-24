c218549f0b0751e8350d82a9ca015308db3ebe02
aryawidiap <79260381+aryawidiap@users.noreply.github.com>
2026-02-24 12:32:58 +0800

    fix formatting

--- gender_blade.py
+++ gender_blade.py
@@ -4,24 +4,31 @@ import pygame as pg
 
 class GenderBlade(tk.Button):
     __slot__ = ['is_pressed']
+    
     def __init__(self, root, note, **kwargs):
         super().__init__(root, command=self.__press_button, background='#f2e6be', **kwargs)
         self.is_pressed: bool = False
         self.note: int = note
         pg.init()
         self.sound = pg.mixer.Sound(f"sounds/note_v_0-9_-0{self.note}.mp3")
+        
     def button_pressed(self):
         self.is_pressed = True
+    
     def button_released(self):
         self.is_pressed = False
+    
     def __press_button(self):
         print(self.note)
         self.play_sound()
+    
     def bind_command(self, func, **kwargs):
         self.configure(command=partial(self.__bind_command, func, **kwargs))
+    
     def __bind_command(self, func, **kwargs):
         func(**kwargs)
         self.__press_button()
+    
     def play_sound(self):
         self.sound.play()
         return
